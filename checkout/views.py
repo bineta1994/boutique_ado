07ebcc0f50c5c0368f60a4225550a67bb611eccf
bineta1994 <bineta1994@gmail.com>
2020-10-02 00:05:07 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HXbFiGKe2byjPbY6qDVyWkJMns7oztAbqTxXbDczH924dwQKaNWFAxb8RhpyntoaZXrLBvDJSgknYgwuE1CkEH2000Kfxypdg',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
